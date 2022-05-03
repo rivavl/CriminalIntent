7b6e12eeb5e74a7d24c60732d3b50c438dde504d
rivavl <mvkalinenkova@yandex.ru>
2022-05-03 21:20:40 +0300

    Add toolbar

--- app/src/main/java/com/marina/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/marina/criminalintent/CrimeListFragment.kt
@@ -4,9 +4,7 @@ package com.marina.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -29,6 +27,11 @@ class CrimeListFragment : Fragment() {
 
     interface Callbacks {
         fun onCrimeSelected(crimeId: UUID)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onAttach(context: Context) {
@@ -64,6 +67,23 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callback = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callback?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
     private fun updateUI(crimes: List<Crime>) {
