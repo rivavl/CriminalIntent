17b37ab125ccb57953cae4038c110226e04d6593
rivavl <91427690+rivavl@users.noreply.github.com>
2022-05-04 14:01:11 +0300

    Implicit Intents (#6)
    * Add strings and buttons
    
    * Add DB migration
    
    * Add string resources for report
    
    * Add intent

--- app/src/main/java/com/marina/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/marina/criminalintent/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.marina.criminalintent.database.CrimeDatabase
+import com.marina.criminalintent.database.migration_1_2
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -11,11 +12,12 @@ private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
 
-    private val database : CrimeDatabase = Room.databaseBuilder(
+    private val database: CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).build()
+    ).addMigrations(migration_1_2)
+        .build()
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
 
@@ -45,8 +47,7 @@ class CrimeRepository private constructor(context: Context) {
         }
 
         fun get(): CrimeRepository {
-            return INSTANCE ?:
-            throw IllegalStateException("CrimeRepository must be initialized")
+            return INSTANCE ?: throw IllegalStateException("CrimeRepository must be initialized")
         }
     }
 }
